3460e03921e78216f72362bcf8ecbbe77b41d678
Mikhail Yohman <mikhail.yohman@gmail.com>
2025-05-22 22:04:20 -0600

    Webhook and example generator

---
+++ generators/add_rack.py
@@ -0,0 +1,15 @@
+from infrahub_sdk.generator import InfrahubGenerator
+
+
+class AddRackGenerator(InfrahubGenerator):
+    """
+    Generator to add a rack to a datacenter.
+    """
+
+    async def generate(self, data: dict):
+        """Generate a request to add a rack to a datacenter.
+
+        Args:
+            data (dict): The data to include in the request.
+        """
+        pass
