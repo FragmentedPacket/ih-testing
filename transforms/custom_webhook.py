a208c7494f2b4a9307923be1bcd80b27c0dd5417
Mikhail Yohman <mikhail.yohman@gmail.com>
2025-05-27 19:11:42 -0600

    Add test schema and comment

--- transforms/custom_webhook.py
+++ transforms/custom_webhook.py
@@ -5,5 +5,6 @@ class CustomWebhook(InfrahubTransform):
     query = "webhook_artifact"
 
     async def transform(self, data):
+        # Test comment
         print(data)
         return {"datas": data}
