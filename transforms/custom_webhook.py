1ad70fb9377abeb8a619b9a2a8645cec112ef918
Mikhail Yohman <mikhail.yohman@gmail.com>
2025-05-27 19:19:49 -0600

    Remove comment.

--- transforms/custom_webhook.py
+++ transforms/custom_webhook.py
@@ -5,6 +5,5 @@ class CustomWebhook(InfrahubTransform):
     query = "webhook_artifact"
 
     async def transform(self, data):
-        # Test comment
         print(data)
         return {"datas": data}
